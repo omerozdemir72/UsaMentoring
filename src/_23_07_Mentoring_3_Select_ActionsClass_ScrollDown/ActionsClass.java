649a0c14185e341cd3b9863e44f2b76dd4d46344
omerozdemir72 <omerozdemir7274@gmail.com>
2020-07-23 21:53:28 +0200

    Actions Class Drag And Drop

--- src/_23_07_Mentoring_3_Select_ActionsClass_ScrollDown/ActionsClass.java
+++ src/_23_07_Mentoring_3_Select_ActionsClass_ScrollDown/ActionsClass.java
@@ -1,10 +1,16 @@
 package _23_07_Mentoring_3_Select_ActionsClass_ScrollDown;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class ActionsClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\omero\\Selenium\\geckodriver.exe");
@@ -13,6 +19,51 @@ public class ActionsClass {
         driver.manage().window().maximize();
 
         driver.get("https://www.etsy.com/");
+
+
+
+        WebElement cookies =driver.findElement(By.cssSelector(".wt-btn.wt-btn--secondary.wt-width-full"));
+
+        cookies.click();
+
+        driver.findElement(By.xpath("//span[@class='wt-display-inline-block wt-vertical-align-middle']")).click();
+
+
+WebElement sprache = driver.findElement(By.xpath("//select[@name='language_code']"));
+
+
+Select slc = new Select(sprache);
+
+slc.selectByValue("en-US");
+
+driver.findElement(By.cssSelector("#locale-overlay-save")).click();
+
+
+Actions actions = new Actions(driver);
+
+List<WebElement> menubar = driver.findElements(By.xpath("//ul[@role='menubar']//li"));
+
+for (WebElement menubars:menubar) {
+    actions.moveToElement(menubars).perform();
+
+    Thread.sleep(500);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/_23_07_Mentoring_3_Select_ActionsClass_ScrollDown/ActionsClass_Drag_Drop.java
+++ src/_23_07_Mentoring_3_Select_ActionsClass_ScrollDown/ActionsClass_Drag_Drop.java
@@ -1,7 +1,10 @@
 package _23_07_Mentoring_3_Select_ActionsClass_ScrollDown;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class ActionsClass_Drag_Drop {
     public static void main(String[] args) {
@@ -14,6 +17,38 @@ public class ActionsClass_Drag_Drop {
 
         driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
 
+
+                            //Capitals
+        WebElement Oslo = driver.findElement(By.id("box1"));
+        WebElement Stockholm = driver.findElement(By.id("box2"));
+        WebElement Washington = driver.findElement(By.id("box3"));
+        WebElement Copenhagen = driver.findElement(By.id("box4"));
+        WebElement Seoul = driver.findElement(By.id("box5"));
+        WebElement Rome = driver.findElement(By.id("box6"));
+        WebElement Madrid = driver.findElement(By.id("box7"));
+
+
+
+        //countries
+
+        WebElement italy = driver.findElement(By.id("box106"));
+        WebElement spain = driver.findElement(By.id("box107"));
+        WebElement  norway= driver.findElement(By.id("box101"));
+        WebElement  denmark= driver.findElement(By.id("box104"));
+        WebElement southKorea = driver.findElement(By.id("box105"));
+        WebElement  sweden= driver.findElement(By.id("box102"));
+        WebElement  unitedStates= driver.findElement(By.id("box103"));
+
+
+        Actions actions = new Actions(driver);
+
+        actions.dragAndDrop(Oslo,norway).build().perform();
+        actions.dragAndDrop(Stockholm,sweden).build().perform();
+        actions.dragAndDrop(Washington,unitedStates).build().perform();
+        actions.dragAndDrop(Copenhagen,denmark).build().perform();
+        actions.dragAndDrop(Seoul,southKorea).build().perform();
+        actions.dragAndDrop(Rome,italy).build().perform();
+        actions.dragAndDrop(Madrid,spain).build().perform();
 
 
 
