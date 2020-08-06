ada1f072d86e68e6bd2f12b00c0185b373a90e03
omerozdemir72 <omerozdemir7274@gmail.com>
2020-08-06 21:06:57 +0200

    random

---
+++ src/b_06_08_Mentoring_6_TestNG_Giris/basit_test2.java
@@ -0,0 +1,32 @@
+package b_06_08_Mentoring_6_TestNG_Giris;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class basit_test2 {
+
+
+    @BeforeClass
+    public void beforeClass(){
+
+        System.out.println("-------------SİSTEM BAŞLATILIYOR.. ---------------------------");
+
+    }
+
+
+    @Test
+    public void Test1(){
+
+        /*
+        int a ve b
+        10 ' a kadar random sayı alsınlar
+        toplamlarını int c ye eşitleyin
+
+         */
+
+    }
+
+
+
+
+}
