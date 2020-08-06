7f3e94dbaf74a39731ce41d8222f93b8c6de7446
omerozdemir72 <omerozdemir7274@gmail.com>
2020-08-06 21:25:19 +0200

    XML File

--- src/b_06_08_Mentoring_6_TestNG_Giris/basit_test2.java
+++ src/b_06_08_Mentoring_6_TestNG_Giris/basit_test2.java
@@ -1,7 +1,11 @@
 package b_06_08_Mentoring_6_TestNG_Giris;
 
+import org.junit.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 public class basit_test2 {
 
@@ -14,16 +18,37 @@ public class basit_test2 {
     }
 
 
-    @Test
-    public void Test1(){
+    @Test(priority = 5)
+    public void Test1() {
+        int a = new Random().nextInt(11);
+        int b = new Random().nextInt(11);
+        int c = a + b;
+        System.out.println("c =  " + c);
+        Assert.assertTrue(c < 12);
 
-        /*
-        int a ve b
-        10 ' a kadar random sayı alsınlar
-        toplamlarını int c ye eşitleyin
+/*  Diğer yolu
+ int a1 = (int) (Math.random()* 10+1);
+ int a2 = (int) (Math.random()* 10+1);
+        */
 
-         */
 
+    }
+    @Test(priority = 2)
+    public void Test2(){
+
+        String a = "Techno Study";
+        String b = "Techno Study";
+
+        Assert.assertEquals(b,a);
+    }
+
+
+
+
+    @AfterClass
+    public void Afterclass(){
+
+        System.out.println("----------------- SİSTEM KAPATILIYOR....... --------------------");
     }
 
 
